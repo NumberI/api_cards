666d0e0c36727fa6cc7781f35b3d4e8999b426ab
NumberI <x0987654321@ya.ru>
2016-04-25 15:22:34 +0300

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  # Api definition
+  namespace :api, defaults: { format: :json },
+                              constraints: { subdomain: 'api' }, path: '/'  do
+
+  end
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
